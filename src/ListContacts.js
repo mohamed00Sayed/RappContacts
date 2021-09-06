594396800fd8a9d79830242ae83b8998d0a0d664
mohamed00Sayed <sayedm22529@gmail.com>
2021-09-06 12:42:43 +0200

    adding Add button and screen toggling

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -25,7 +25,7 @@ class ListContacts extends Component{
 	render(){
 		
 		const { query } = this.state
-		const { contacts, onDeleteContact } = this.props
+		const { contacts, onDeleteContact, onNavigate } = this.props
 
 		const showingContacts = query === ''
 		  ? contacts
@@ -42,6 +42,12 @@ class ListContacts extends Component{
 						value={query}
 						onChange={(evt) => {this.updateQuery(evt.target.value)}}
 					/>
+					
+					<a 
+						href='#create'
+						onClick={onNavigate}
+						className='add-contact'
+					>Add Contact</a>
 				</div>
 				
 				{showingContacts.length !== contacts.length && (
