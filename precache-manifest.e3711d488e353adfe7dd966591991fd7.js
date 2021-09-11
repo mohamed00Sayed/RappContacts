22e5e818dd85daf1c8eb4e7faa4d5a6f978c1c15
mohamed00Sayed <87094481+mohamed00Sayed@users.noreply.github.com>
2021-09-11 08:53:32 +0000

    Deploying to gh-pages from @ mohamed00Sayed/RappContacts@dc625f70dd18457be9b94804458ef5ddb04784b2 🚀

---
+++ precache-manifest.e3711d488e353adfe7dd966591991fd7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "6351df7ea9bbf68a972a",
+    "url": "/static/css/main.572b9ec7.chunk.css"
+  },
+  {
+    "revision": "6351df7ea9bbf68a972a",
+    "url": "/static/js/main.6351df7e.chunk.js"
+  },
+  {
+    "revision": "6afee383564df150508c",
+    "url": "/static/js/1.6afee383.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c0c5c509a7483ce0b8c11ca04f3633f1",
+    "url": "/static/media/arrow-back.c0c5c509.svg"
+  },
+  {
+    "revision": "1456a6be906dcfe7818c47b28b409501",
+    "url": "/static/media/person.1456a6be.svg"
+  },
+  {
+    "revision": "0fc053ba9ccf6f9d63d45098678875bf",
+    "url": "/static/media/search.0fc053ba.svg"
+  },
+  {
+    "revision": "c9c2fd159a339e59c77962878b1676bd",
+    "url": "/static/media/person-add.c9c2fd15.svg"
+  },
+  {
+    "revision": "576f26ff09a3275d9da0621e6b34898d",
+    "url": "/static/media/cancel.576f26ff.svg"
+  },
+  {
+    "revision": "dd7ff5f4a36fd143bb36ee8f2ea2b17b",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/RappContacts/precache-manifest.453a5f64d73a0df2514599dbd2ae70fd.js"
+  "/precache-manifest.e3711d488e353adfe7dd966591991fd7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/RappContacts/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
