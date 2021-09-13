99ab4c2b11f615753cd7dfbfb76c054566f243e6
mohamed00Sayed <87094481+mohamed00Sayed@users.noreply.github.com>
2021-09-13 18:11:17 +0000

    Deploying to gh-pages from @ mohamed00Sayed/RappContacts@2e1a3fe74ccf279c4f8187788479d6409d5983ae 🚀

---
+++ precache-manifest.ae6f572b33adbfec59ae85f3c1259452.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2300ef177ba1e8ecd62",
+    "url": "/RappContacts/static/css/main.1a98da33.chunk.css"
+  },
+  {
+    "revision": "b2300ef177ba1e8ecd62",
+    "url": "/RappContacts/static/js/main.b2300ef1.chunk.js"
+  },
+  {
+    "revision": "6afee383564df150508c",
+    "url": "/RappContacts/static/js/1.6afee383.chunk.js"
+  },
+  {
+    "revision": "13017abda9437554e9c4",
+    "url": "/RappContacts/static/js/runtime~main.13017abd.js"
+  },
+  {
+    "revision": "c0c5c509a7483ce0b8c11ca04f3633f1",
+    "url": "/RappContacts/static/media/arrow-back.c0c5c509.svg"
+  },
+  {
+    "revision": "1456a6be906dcfe7818c47b28b409501",
+    "url": "/RappContacts/static/media/person.1456a6be.svg"
+  },
+  {
+    "revision": "0fc053ba9ccf6f9d63d45098678875bf",
+    "url": "/RappContacts/static/media/search.0fc053ba.svg"
+  },
+  {
+    "revision": "c9c2fd159a339e59c77962878b1676bd",
+    "url": "/RappContacts/static/media/person-add.c9c2fd15.svg"
+  },
+  {
+    "revision": "576f26ff09a3275d9da0621e6b34898d",
+    "url": "/RappContacts/static/media/cancel.576f26ff.svg"
+  },
+  {
+    "revision": "c5b244e8e5c1d5a69963605feb1a201e",
+    "url": "/RappContacts/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e3711d488e353adfe7dd966591991fd7.js"
+  "/RappContacts/precache-manifest.ae6f572b33adbfec59ae85f3c1259452.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/RappContacts/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
